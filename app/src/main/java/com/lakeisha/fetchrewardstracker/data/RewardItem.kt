e325a804f7edbaa7a01662113b082822d5c8e152
Lakeisha McCree <lmccree215@icloud.com>
2026-05-15 10:22:02 -0400

    Convert RewardItem into Room entity

--- app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardItem.kt
+++ app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardItem.kt
@@ -1,6 +1,12 @@
 package com.lakeisha.fetchrewardstracker.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "rewards")
 data class RewardItem(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title:String,
     val points: Int,
