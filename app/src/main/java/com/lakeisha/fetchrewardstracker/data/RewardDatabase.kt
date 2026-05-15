1748f65a8d3c608e4dbc7eef23fb13446b8d1975
Lakeisha McCree <lmccree215@icloud.com>
2026-05-15 10:33:49 -0400

    Create Room database configuration

--- app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardDatabase.kt
+++ app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardDatabase.kt
@@ -1,4 +1,35 @@
 package com.lakeisha.fetchrewardstracker.data
 
-class RewardDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [RewardItem::class],
+    version = 1
+)
+abstract class RewardDatabase : RoomDatabase() {
+
+    abstract fun rewardDao(): RewardDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RewardDatabase? = null
+
+        fun getDatabase(context: Context): RewardDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RewardDatabase::class.java,
+                    "reward_database"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
 }
