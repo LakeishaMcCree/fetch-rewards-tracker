607514d601033fbedd6b8651e032926b3a844474
Lakeisha McCree <lmccree215@icloud.com>
2026-05-15 10:28:01 -0400

    Add RewardDao database operations

--- app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardDao.kt
+++ app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardDao.kt
@@ -1,4 +1,24 @@
 package com.lakeisha.fetchrewardstracker.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface RewardDao {
+    @Query("SELECT * FROM rewards ORDER BY id desc")
+    fun getAllRewards(): Flow<List<RewardItem>>
+
+    @Insert
+    suspend fun insertReward(reward: RewardItem)
+
+    @Update
+    suspend fun updateReward(reward: RewardItem)
+
+    @Delete
+    suspend fun deleteReward(reward: RewardItem)
 }
---
+++ app/src/main/java/com/lakeisha/fetchrewardstracker/data/RewardDatabase.kt
@@ -0,0 +1,4 @@
+package com.lakeisha.fetchrewardstracker.data
+
+class RewardDatabase {
+}
